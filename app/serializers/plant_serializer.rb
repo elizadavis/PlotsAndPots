6b1ecc06d3d178ec5ca83755c3381678036e93d2
elizadavis <eelizadavis@gmail.com>
2019-06-11 08:58:43 -0400

    Add editable to plant serializer
    Added editable method and to serializer and its attributes

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,5 +1,5 @@
 class PlantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :plant_type, :editable
+  attributes :id, :name, :location, :plant_type
   has_one :user
 
   def editable
